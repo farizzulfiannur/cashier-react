8cffe6ae979edb981656485caf80df352310f143
farizzulfiannur <farizzulfiannur@gmail.com>
2023-08-16 21:31:57 +0700

    installation package

---
+++ src/components/Cobak.js
@@ -0,0 +1,79 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function Navbars() {
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          {/* <Navbar.Text> */}
+          {/* Signed in as: <a href="#login">Mark Otto</a> */}
+          <Nav className="justify-content-end" activeKey="/home">
+            <Nav.Item>
+              <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="disabled" disabled>
+                Disabled
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          {/* </Navbar.Text> */}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    // <Navbar expand="lg" className="bg-body-secondary">
+    //   <Container>
+    //     <Navbar.Brand href="#">Percobaan React JS</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="navbarScroll" />
+    //     <Navbar.Collapse id="navbarScroll">
+    //   <Nav
+    //     className="me-auto my-2 my-lg-0"
+    //     style={{ maxHeight: '100px' }}
+    //     navbarScroll
+    //   >
+    //     <Nav.Link href="#action1">Home</Nav.Link>
+    //     <Nav.Link href="#action2">Link</Nav.Link>
+    //     <NavDropdown title="Link" id="navbarScrollingDropdown">
+    //       <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+    //       <NavDropdown.Item href="#action4">
+    //         Another action
+    //       </NavDropdown.Item>
+    //       <NavDropdown.Divider />
+    //       <NavDropdown.Item href="#action5">
+    //         Something else here
+    //       </NavDropdown.Item>
+    //     </NavDropdown>
+    //     <Nav.Link href="#" disabled>
+    //       Link
+    //     </Nav.Link>
+    //   </Nav>
+    //       <Form className="d-flex">
+    //         <Form.Control
+    //           type="search"
+    //           placeholder="Search"
+    //           className="me-2"
+    //           aria-label="Search"
+    //         />
+    //         <Button variant="outline-success">Search</Button>
+    //       </Form>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
+  );
+}
+
+export default Navbars;
